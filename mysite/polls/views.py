5f5b6e519590a0099c1a0080c52ad82a1714eaa8
constantino <muheec@gmail.com>
2020-04-26 19:09:04 -0700

    success!

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,6 +9,30 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Choice, Question
 
+from .forms import QuestionCreateForm, ChoiceCreateForm
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+
+class QuestionCreateView(LoginRequiredMixin, generic.edit.CreateView):
+    login_url = reverse_lazy('login')
+    def get(self, request, *args, **kwargs):
+        context = {
+          'form': QuestionCreateForm()
+        }
+        return render(request, 'polls/create.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = QuestionCreateForm(request.POST)
+        if form.is_valid:
+            question = form.save(commit=False) # don't save the question yet
+            question.author = request.user
+            question.save()
+            return HttpResponseRedirect(
+                reverse('polls:detail', args=[question.id]))
+        # else if form is not valid
+        return render(request, 'polls/create.html', { 'form': form })
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
@@ -25,11 +49,25 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['choice_form'] = ChoiceCreateForm()
+        return context
+
+    def post(self, request, pk):
+        form = ChoiceCreateForm(request.POST)
+        if form.is_valid:
+            choice = form.save(commit=False)
+            choice.question = Question.objects.get(pk=pk)
+            choice.save()
+            return HttpResponseRedirect(reverse('polls:detail', args=[pk]))
+        # else if form is not valid
+        context = {
+          'choice_form': form,
+          'question': Question.objects.get(pk=pk)
+        }
+        return render(request, 'polls/detail.html', context)
 
 class ResultsView(generic.DetailView):
     model = Question
